b9d6517c36908bf10249143adcf585e21e7ca731
CoderDuck <64244383+HongDongk@users.noreply.github.com>
2023-09-16 15:57:48 +0900

    Time: 51 ms (75.21%), Space: 41.7 MB (63.07%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    let newArr = [0, 1]; //0번째 1번째 요소는 고정
+
+    function fibonacci(n){
+        if (newArr[n] !== undefined){ 
+            return newArr[n]; //이미 있는 건 그대로 리턴
+        }
+        newArr[n] = fibonacci(n - 1) + fibonacci(n - 2); // 없는 건 새로 만들어서 저장!!!
+        return newArr[n];
+    };
+
+    return fibonacci(n);
+    
+};
